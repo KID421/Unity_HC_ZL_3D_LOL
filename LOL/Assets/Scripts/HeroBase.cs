c9479b64c77e214063ba17a9c12fa6357a551532
KID <50666207+KID421@users.noreply.github.com>
2020-12-26 21:01:20 +0800

    技能冷卻

--- LOL/Assets/Scripts/HeroBase.cs
+++ LOL/Assets/Scripts/HeroBase.cs
@@ -25,6 +25,29 @@ public class HeroBase : MonoBehaviour
         ani = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        TimerControl();
+    }
+
+    private void TimerControl()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (skillStart[i])
+            {
+                skillTimer[i] += Time.deltaTime;
+
+                // 如果 計時器 >= 冷卻時間 就 歸零並且設定為 尚未開始
+                if (skillTimer[i] >= data.skills[i].cd)
+                {
+                    skillTimer[i] = 0;
+                    skillStart[i] = false;
+                }
+            }
+        }
+    }
+
     public void Move()
     {
 
@@ -32,21 +55,30 @@ public class HeroBase : MonoBehaviour
 
     public void Skill1()
     {
+        // 如果 技能已經開始 就跳出
+        if (skillStart[0]) return;
         ani.SetTrigger("第一招");
+        skillStart[0] = true;
     }
 
     public void Skill2()
     {
+        if (skillStart[1]) return;
         ani.SetTrigger("第二招");
+        skillStart[1] = true;
     }
 
     public void Skill3()
     {
+        if (skillStart[2]) return;
         ani.SetTrigger("第三招");
+        skillStart[2] = true;
     }
 
     public void Skill4()
     {
+        if (skillStart[3]) return;
         ani.SetTrigger("大絕");
+        skillStart[3] = true;
     }
 }
