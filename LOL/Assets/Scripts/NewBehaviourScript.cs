04c8c9b417f487e6a047b805ad7b449262498e56
KID <50666207+KID421@users.noreply.github.com>
2021-01-09 21:31:14 +0800

    備份

---
+++ LOL/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void GetInput(string getInput)
+    {
+        int number = Convert.ToInt32(getInput);
+
+        print(number);
+    }
+}
